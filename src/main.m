ecf1b8f52fe0381cf17bd2a144dbe747ef50b4ab
Maximo <maximovs@gmail.com>
2013-04-15 14:52:11 -0300

    comentarios

--- src/main.m
+++ src/main.m
@@ -4,10 +4,12 @@ function net = main(beta,inputSize,op,txFun,lrnRate, epochs)
 %W = zeros(inputSize+1,1);
 for j = 1:epochs
     for i = 1:size(trainSet)
-  %  o = sign([-1 trainSet{i}{1}]*W);
-    o = feval(txFun,[-1 trainSet{i}{1}]*W,beta);
 
+    %calculas los o de cada capa
+    o = feval(txFun,[-1 trainSet{i}{1}]*W,beta);
+    %calculas los delta de cada capa
     dW = [-1 trainSet{i}{1}]*lrnRate*(trainSet{i}{2} - o);
+    %updateas los valores de cada capa
     W = W+dW';
     end
 end
